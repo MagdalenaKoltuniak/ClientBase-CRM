fbcce30cbb9bee3477ab809f3d6e7ca9b5a91052
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2023-03-07 20:12:05 +0100

    add CRUD operations

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const hbs = require('express-handlebars');
 const { urlencoded } = require('express');
 const { clientRouter } = require('./routers/client');
 const { homeRouter } = require('./routers/home');
+const { db } = require('./utils/db');
 
 const app = express();
 
@@ -24,6 +25,12 @@ app.set('view engine', 'hbs');
 
 app.use('/', homeRouter);
 app.use('/client', clientRouter);
+app.get('/test', (req, res) => {
+	db.update('b1f3c618-df84-422a-be94-f29ef06d888e', {
+		name: 'Tester',
+	});
+	res.send('ok');
+});
 
 app.listen(3000, '0.0.0.0', () => {
 	console.log('Listening on server');
